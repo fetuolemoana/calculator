114479437af39f5550fe999e5b09478acf8ef236
fetuolemoana <fetu.tamapeau@gmail.com>
2019-01-04 15:57:48 +1300

    Clear index and css content

---
+++ calculator.js
@@ -0,0 +1,14 @@
+
+
+// Variables, probably arrays to store calculator input data 
+
+// Calculator click 
+
+
+
+  console.log('button click'); 
+
+
+  // All the functions that will calculate input data 
+
+
