40ed285e58ed2e9a7069df9714b53df59f768650
fetuolemoana <fetu.tamapeau@gmail.com>
2019-01-08 19:26:10 +1300

    Add function to recognise number key

--- calculator.js
+++ calculator.js
@@ -2,13 +2,22 @@
 
 // Variables, probably arrays to store calculator input data 
 
-// Calculator click 
+// Insert event listener for calclator button click 
 
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
 
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+   const key = e.target 
+   const action = key.dataset.action 
 
+   if (!action) {
+     console.log('number key!') 
+   }
+ }
+})
 
-
-
-  // All the functions that will calculate input data 
+  // All the functions that will handle input data 
 
 
